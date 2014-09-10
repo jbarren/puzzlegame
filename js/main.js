9f1f2760f1b0d3a82c3fdaab7775aaa1f16d4efd
Jonatan Barrenechea <jbarren@gmail.com>
2014-09-10 18:27:04 +0100

    Removed jshint issues

--- js/main.js
+++ js/main.js
@@ -1,97 +1,96 @@
-'use strict';
 var PUZZLE_GAME = (function () {
-    function StageArea(stage) {
-        //the bigger this value, the slower the falling speed
-        var speed = 50;
-        var speedCounter = 0;
-        var area = (function () {
-            var areaArray = new Array(10);
-            for (var i = 0; i < 10; i++) {
-                areaArray[i] = new Array(10);
-            }
-            return areaArray;
-        }());
+  'use strict';
+  function StageArea(stage) {
+      //the bigger this value, the slower the falling speed
+      var speed = 50;
+      var speedCounter = 0;
+      var area = (function () {
+          var areaArray = new Array(10);
+          for (var i = 0; i < 10; i++) {
+              areaArray[i] = new Array(10);
+          }
+          return areaArray;
+      }());
 
-        var pieceInMovement = new Piece();
+      var pieceInMovement = new Piece();
 
-        function Piece() {
-            var texture = PIXI.Texture.fromImage("../images/square.png");
-            var image = new PIXI.Sprite(texture);
-            var x = 5;
-            var y = 0;
-            area[y][x] = this;
-            image.position.x = x * 50;
-            image.position.y = y * 50;
-            stage.addChild(image);
-            return {
-                getX: function () {
-                    return x;
-                },
-                getY: function () {
-                    return y;
-                },
-                moveDown: function () {
-                    area[y][x] = undefined;
-                    y++;
-                    area[y][x] = this;
-                    image.position.y = y * 50;
-                },
-                moveLeft: function () {
-                    area[y][x] = undefined;
-                    x--;
-                    area[y][x] = this;
-                    image.position.x = x * 50;
-                },
-                moveRight: function () {
-                    area[y][x] = undefined;
-                    x++;
-                    area[y][x] = this;
-                    image.position.x = x * 50;
-                }
-            };
-        };
+      function Piece() {
+          var texture = PIXI.Texture.fromImage("../images/square.png");
+          var image = new PIXI.Sprite(texture);
+          var x = 5;
+          var y = 0;
+          area[y][x] = this;
+          image.position.x = x * 50;
+          image.position.y = y * 50;
+          stage.addChild(image);
+          return {
+              getX: function () {
+                  return x;
+              },
+              getY: function () {
+                  return y;
+              },
+              moveDown: function () {
+                  area[y][x] = undefined;
+                  y++;
+                  area[y][x] = this;
+                  image.position.y = y * 50;
+              },
+              moveLeft: function () {
+                  area[y][x] = undefined;
+                  x--;
+                  area[y][x] = this;
+                  image.position.x = x * 50;
+              },
+              moveRight: function () {
+                  area[y][x] = undefined;
+                  x++;
+                  area[y][x] = this;
+                  image.position.x = x * 50;
+              }
+          };
+      }
 
-        return {
-            moveDown: function () {
-                if (speedCounter < speed) {
-                    speedCounter++;
-                    return true;
-                }
-                speedCounter = 0;
-                if (pieceInMovement.getY() >= 9
-                    || area[pieceInMovement.getY() + 1][pieceInMovement.getX()]) {
-                    return false;
-                }
-                pieceInMovement.moveDown();
-                return true;
-            },
-            moveLeft: function (){
-                if(pieceInMovement.getX() <= 0 || area[pieceInMovement.getY()][pieceInMovement.getX() - 1]){
-                    return false;
-                }
-                pieceInMovement.moveLeft();
-                input.keyPressedAlready = true;
-                input.left = false;
-                return true;
-            },
-            moveRight: function (){
-                if(pieceInMovement.getX() >= 9 || area[pieceInMovement.getY()][pieceInMovement.getX() + 1]){
-                    return false;
-                }
-                pieceInMovement.moveRight();
-                input.keyPressedAlready = true;
-                input.right = false;
-                return true;
-            },
-            createNewPiece: function () {
-                if (area[0][5]) {
-                    return false;
-                }
-                pieceInMovement = new Piece();
-                return true;
-            }
-        }
-    };
+      return {
+          moveDown: function () {
+              if (speedCounter < speed) {
+                  speedCounter++;
+                  return true;
+              }
+              speedCounter = 0;
+              if (pieceInMovement.getY() >= 9 || area[pieceInMovement.getY() + 1][pieceInMovement.getX()]) {
+                  return false;
+              }
+              pieceInMovement.moveDown();
+              return true;
+          },
+          moveLeft: function (){
+              if(pieceInMovement.getX() <= 0 || area[pieceInMovement.getY()][pieceInMovement.getX() - 1]){
+                  return false;
+              }
+              pieceInMovement.moveLeft();
+              input.keyPressedAlready = true;
+              input.left = false;
+              return true;
+          },
+          moveRight: function (){
+              if(pieceInMovement.getX() >= 9 || area[pieceInMovement.getY()][pieceInMovement.getX() + 1]){
+                  return false;
+              }
+              pieceInMovement.moveRight();
+              input.keyPressedAlready = true;
+              input.right = false;
+              return true;
+          },
+          createNewPiece: function () {
+              if (area[0][5]) {
+                  return false;
+              }
+              pieceInMovement = new Piece();
+              return true;
+          }
+      };
+  }
 
     var stage;
     var renderer;
@@ -171,4 +170,3 @@ var PUZZLE_GAME = (function () {
 
     start();
 }());
-
