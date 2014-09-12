d984ab1e1d474c540e295e4af46b2c793e2d8e10
Jonatan Barrenechea <jbarren@gmail.com>
2014-09-12 22:57:03 +0100

    Livereload working, so main page refreshed automatically every time a file is changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,14 +12,30 @@ module.exports = function(grunt){
       }
     },
     watch: {
-      files: ['js/*.js', 'tests/**/*.js'],
-      tasks: ['mocha']
+      files: ['html/*.html', 'js/*.js', 'tests/**/*.js'],
+      tasks: ['mocha'],
+      options: {
+        livereload: true
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: '.',
+          livereload: true
+        }
+      }
     }
   });
 
   // Load grunt mocha task
   grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
 
   grunt.registerTask('default', ['mocha']);
+  grunt.registerTask('server', "Serve your app", ['connect', 'watch' ]);
+
 };
